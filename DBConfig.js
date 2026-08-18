102dffd799537e2b1bb0d74d668ed932010032db
Jose Flavio <flaviopereiralc@gmail.com>
2026-08-18 11:22:53 -0300

    initial commit

---
+++ DBConfig.js
@@ -0,0 +1,10 @@
+import { Pool } from "pg";
+
+const itemsPool = new Pool({
+  connectionString: process.env.DBConfigLink,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+export default itemsPool
